7443377da5c74952eb4422fed51e494a29c9eaba
xDouglas90 <xdouglas90@gmail.com>
2021-12-11 06:06:07 -0300

    Added props and functionalities to open/close Modal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/Global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from "react";
 
 export function App() {
+  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionsModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionsModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
