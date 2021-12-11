921e70e5561b60aaefccbdbb29c1647264930c37
xDouglas90 <xdouglas90@gmail.com>
2021-12-11 00:43:23 -0300

    Added Poppins custom font.

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -40,6 +40,15 @@ export const GlobalStyle = createGlobalStyle`
     background: var(--bg-color);
   }
 
+  body, input, textarea, button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  } 
+
   button {
     cursor: pointer;
   }
