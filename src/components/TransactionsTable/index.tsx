4c6a75b261b8061714e78d81daf558543dd32fa0
xDouglas90 <xdouglas90@gmail.com>
2021-12-11 04:00:26 -0300

    Added API mocking lib MirageJS to the project.

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,44 +1,51 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-    return (
-      <Container>
-          <table>
-              <thead>
-                  <tr>
-                      <th>Título</th>
-                      <th>Valor</th>
-                      <th>Categoria</th>
-                      <th>Data</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>Salário</td>
-                      <td className="active">R$ 1.500</td>
-                      <td>Estágio</td>
-                      <td>10/12/2021</td>
-                  </tr>
-                  <tr>
-                      <td>Aluguel</td>
-                      <td className="passive">R$ -200,00</td>
-                      <td>Moradia</td>
-                      <td>10/12/2021</td>
-                  </tr>
-                  <tr>
-                      <td>Ticket</td>
-                      <td className="active">R$ 425,00</td>
-                      <td>Vale Refeição</td>
-                      <td>30/11/2021</td>
-                  </tr>
-                  <tr>
-                      <td>Oi Fibra</td>
-                      <td className="passive">R$ -119,00</td>
-                      <td>Internet</td>
-                      <td>10/12/2021</td>
-                  </tr>
-              </tbody>
-          </table>
-      </Container>
-    )
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Salário</td>
+            <td className="active">R$ 1.500</td>
+            <td>Estágio</td>
+            <td>10/12/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="passive">R$ -200,00</td>
+            <td>Moradia</td>
+            <td>10/12/2021</td>
+          </tr>
+          <tr>
+            <td>Ticket</td>
+            <td className="active">R$ 425,00</td>
+            <td>Vale Refeição</td>
+            <td>30/11/2021</td>
+          </tr>
+          <tr>
+            <td>Oi Fibra</td>
+            <td className="passive">R$ -119,00</td>
+            <td>Internet</td>
+            <td>10/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
